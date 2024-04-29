21c8c34f7686732a0167db231d2cba0527dcec69
Brian Waobikeze <48830488+bwaobikeze@users.noreply.github.com>
2024-04-29 16:03:16 -0500

    Update fuelQuoteModule.js
    Removed un needed functions and comments

--- src/fuelQuoteModule.js
+++ src/fuelQuoteModule.js
@@ -17,52 +17,7 @@ class fuelQuoteModule {
     tempQ.SetAdress(adresPassed);
     return tempQ;
   }
-
-  //add a quote to the users account(enter object and store in a Linked List the new quote)
-  /*
-  Explanation for function UCClienQuoteManagement():
-  takes in the user email the newly created Quote object and the user list(this can change when we get the database up and running)
-  and searches through the user database list and then once we find the user email that we also 
-  passed in we retrieve that users quote history list and push/add the latest quote object into the users UserHistory list  breaks out of the loop 
-   */
-  // UCClienQuoteManagement(UCQuote, userID, UserDBLIst) {
-  //   var UserDBLIst;
-  //   for (let i = 0; i < UserDBLIst.length; i++) {
-  //     if (userID == UserDBLIst[i].id) {
-  //       ListOfUsers[i].UserHistory.push(UCQuote);
-  //       break;
-  //     }
-  //   }
-  // }
-
-  //displays quotes in a tabular display(this should return data in a format that the front end team can display anyway they like.
-  /*
-  Explanation for function UCClientHistory():
-  takes in the user email and the user list(this can change when we get the database up and running)
-  and searches through the user database list and then once we find the user email that we also 
-  passed in we retrieve that users quote history list breaks out of the loop then we iterate over
-  the users quote history and print them to the console(This can be a good way to get each property of the quote class and display 
-  each of them with each quote) 
-   */
-  /**
-   *
-   * @param {*} userEmail
-   * @param {*} UserDBLIst
-   */
-  // UCClientHistory(userEmail, UserDBLIst) {
-  //   var UserDBLIst = [];
-  //   var returnedUser;
-  //   for (let i = 0; i < ListOfUsers.length; i++) {
-  //     if (userEmail == UserDBLIst[i].email) {
-  //       returnedUser = UserDBLIst[i];
-  //       break;
-  //     }
-  //   }
-  //   returnedUser.UserHistory.forEach((element) => {
-  //     Console.log(element);
-  //   });
-  // }
-
+  
   //what profit margin we want calculate the quote for the user
   UCPricingTotal(userQuote, UserID) {
     const LocationFactTexas = 0.02;
